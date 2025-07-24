a76436bfe8807cd6edad2695fa86bab172d1e2b0
Junfel <junfeldev@gmail.com>
2025-07-24 22:26:03 +0800

    Update MD, Added footer.

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,10 +1,11 @@
 <script setup>
+import BestSeller from '@/components/BestSeller.vue';
 import BannerComponent from '../components/BannerComponent.vue'
 import HighlightsComponent from '../components/HighlightsComponent.vue'
 
 const bannerProps = {
   title: 'CapCAKES',
-  tagline: 'DreamCakes is a cozy neighborhood spot where artistry meets indulgence. From elegantly crafted birthday cakes to whimsical custom designs, each creation is baked fresh using premium ingredients and a whole lot of love.',
+  tagline: 'Is a cozy neighborhood spot where artistry meets indulgence. From elegantly crafted birthday cakes to whimsical custom designs, each creation is baked fresh using premium ingredients and a whole lot of love.',
   destination: 'Products',
   buttonLabel: 'View Our Products'
 }
@@ -13,6 +14,7 @@ const bannerProps = {
 <template>
   <div class="container-fluid text-white pb-5">
     <BannerComponent :bannerProps=bannerProps />
+    <BestSeller />
     <HighlightsComponent />
   </div>
 </template>
