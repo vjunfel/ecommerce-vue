17405a8b8646a1559d9b6ccace6fc971e7f8bae4
Junfel <junfeldev@gmail.com>
2025-07-25 14:19:58 +0800

    Added cart page

---
+++ src/pages/CartPage.vue
@@ -0,0 +1,14 @@
+<template>
+	<div class="container-fluid row">
+		<div class="col my-5 text-center">
+			<h1 class="text-center text-primary py-1">CART</h1>
+			<!-- <p class="text-center">Select a product to enroll in!</p> -->
+       <div class="text-center py-5">
+        <div class="my-5">
+          <h2>Cart Items</h2>
+        </div>
+        <button class="btn btn-warning px-4 py-2">Check out</button>
+       </div>
+		</div>
+	</div>
+</template>
