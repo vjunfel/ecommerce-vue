3986dcff3c3612809eb3b2fe0c0d3dbbe7b9cf02
Junfel <junfeldev@gmail.com>
2025-07-26 15:48:44 +0800

    Added best selling products in homepage

--- src/components/BestSeller.vue
+++ src/components/BestSeller.vue
@@ -1,8 +1,76 @@
+<script setup>
+import { ref, onMounted } from "vue";
+import "notyf/notyf.min.css";
+import api from "@/api";
+import BestSellerProduct from "./BestSellerProduct.vue";
+
+const products = ref([]);
+const loading = ref(false);
+
+const bestSellerItem = async () => {
+  loading.value = true;
+	try {
+		const res = await api.get("/products/active");
+    
+    if(res.status === 200) {
+      const bestSeller = res.data.filter((item) => {
+        return (item.bestseller)
+      })
+      products.value = bestSeller || [];
+    }
+	} catch (error) {
+		console.error(error);
+	} finally {
+		loading.value = false;
+	}
+};
+
+onMounted( bestSellerItem );
+</script>
+
 <template>
-  <!-- <div id="banner" class="row mb-5 py-5   row d-flex justify-content-center" >
-    <div id="content" class="content col-md-8 p-5 ">
-      <h1 class="mb-3" id="banner-title">Best Seller</h1>
-      <p class="mb-5" id="motto"></p>
+	<section class="container">
+    <div class="col my-5">
+      <h1 class="text-center text-dark pt-5 pb-3">Best Sellers</h1>
+		</div>
+		
+		<div class="text-center p-5 text-dark" v-if="loading"> 
+			<div class="spinner-border m-3 text-warning"></div>
+			<p>Loading...</p>
+		</div>
+
+    <div v-else="!loading" class="row g-4 mb-5">
+      <BestSellerProduct
+        v-for="product in products"
+        :key="product._id"
+        :productData="product"
+      />
     </div>
-  </div> -->
+	</section>
 </template>
+
+<style scoped>
+.custom-search {
+	display: flex;
+	align-items: center;
+	justify-content: center;
+}
+
+.custom-form {
+	max-width: 800px;
+	margin-bottom: 1rem;
+	margin-bottom: 2rem;
+}
+
+.list-group-item + .list-group-item {
+	margin-top: 0.5rem;
+}
+
+.search-results {
+	margin-top: 2rem;
+	border-left: 5px solid #ffc107; 
+	background-color: #fef9e7;
+}
+</style>
+
+
---
+++ src/components/BestSellerProduct.vue
@@ -0,0 +1,54 @@
+<script setup>
+import imgSource from "@/assets/images/Image-placeholder-item.jpg"
+
+defineProps({
+  productData: Object
+})
+
+</script>
+
+<template>
+	<section class="col-12 col-md-6 col-lg-4 my-3">
+		<div id="CourseCard" class="card cardHighlights shadow-sm" style="min-height: 100%;">
+			<router-link :to="{path:`/products/${productData._id}`}">
+				<img 
+					style="height: 280px; object-fit: cover;"
+					class="card-img-top img-fluid"
+					:src="productData.src ? productData.src : imgSource"
+					:alt="productData.name"
+				>
+			</router-link>
+			<div class="card-body">
+				<h4 class="card-title fw-bold mb-2">{{productData.name}}</h4>
+				<p class="card-text text-muted mb-2">
+					{{productData.description.slice(0, 100) + (productData.description.length > 100 ? '...' : '')}}
+				</p>
+				
+				<p class="mb-1">
+					<span class="fw-semibold">Price:</span> PHP {{productData.price}}
+				</p>
+				
+			</div>
+			<div class="d-grid mt-2 m-3 position-sticky">
+				<router-link class="btn-custom d-block" :to="{path:`/products/${productData._id}`}">View Product</router-link>
+			</div>
+		</div>
+	</section>
+</template>
+
+<style scoped>
+	.btn-custom {
+		outline: 1px solid hsl(46, 100%, 48%);
+		color: #333;
+		padding: 8px;
+		text-align: center;
+		border-radius: 5px;
+		text-decoration: none;
+		font-weight: 400;
+	}
+	.btn-custom:hover {
+		background-color: hsl(46, 100%, 55%);
+		text-align: center;
+		border-radius: 5px;
+	}
+</style>
