4612ffaccde14705155bd878d72382bc9d3010dc
Junfel <junfeldev@gmail.com>
2025-08-01 00:50:14 +0800

    Added api-base-URL

--- src/api.js
+++ src/api.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 
-const api = axios.create({
-    baseURL: import.meta.env.VITE_ECOMMERCE_API,
-});
+const api = axios.create({ baseURL: "https://vvro2vmufk.execute-api.us-west-2.amazonaws.com/production/" });
 
 api.interceptors.request.use((config) => {
   
