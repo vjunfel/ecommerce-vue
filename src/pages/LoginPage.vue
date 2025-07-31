03940d98865441628cd2fc3bb92ea5738b8b7dfb
Junfel <junfeldev@gmail.com>
2025-08-01 01:29:47 +0800

    Trying to fix login

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -5,8 +5,8 @@ import { useUserStore } from "@/stores/userStore";
 import Swal from 'sweetalert2'
 import api from "@/api";
 
-const email = ref("");
-const password = ref("");
+const email = ref("test@mail.com");
+const password = ref("asdf123");
 const isEnabled = ref(false);
 
 const userStore = useUserStore();
@@ -21,22 +21,24 @@ watch([email, password], (currentValue, oldValue) => {
 });
 
 const handleSubmit = async () => {
-  try {
-    // await userStore.loginUser({ 
-		// 	email: email.value, 
-		// 	password: password.value 
-		// });
-		
-		await api.post("/users/login", { 
+  try {	
+		const res = await api.post("/users/login", { 
 			email: email.value, 
 			password: password.value
 		});
 		
+		console.log("RES", res);
+		
+		if (res.data.status === 200) {
+			await userStore.loginUser({ 
+				email: email.value, 
+				password: password.value 
+			});
+		}
 		
 		await userStore.fetchUserDetails();
 		
 		userStore.isAdmin ? router.push('/dashboard') : router.push('/');
-    // router.push('/');
 		
   } catch (error) {
 		console.log("ERROR", error)
