9da5e39c26258edf541b04e1393ca37eaa54cd3a
Junfel <junfeldev@gmail.com>
2025-08-02 06:19:36 +0800

    Fix login

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -20,33 +20,74 @@ watch([email, password], (currentValue, oldValue) => {
 	}
 });
 
-const handleSubmit = async () => {
-  try {	
-		const res = await api.post("/users/login", { 
-			email: email.value, 
-			password: password.value
-		});
+const loginWithFetch = async () => {	
+	try {
+		// const response = await fetch("http://localhost:4000/users/login", {
+		const response = await fetch("https://vvro2vmufk.execute-api.us-west-2.amazonaws.com/production/users/login", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json"
+			},
+			body: JSON.stringify({
+				email: email.value,
+				password: password.value
+			})
+		})
 		
-		console.log("LOGIN RES =======> ", res);
+		console.log("LOGIN FETCH RES =======> ", response);
+		console.log("OK:", response.ok);
 		
-		if (!res.status === 200) {
-			throw new Error("Failed to login")
+		if (!response.ok) {
+			throw new Error(`HTTP error! status: ${response.status}`)
 		}
 		
-		await userStore.loginUser({ 
-			email: email.value, 
-			password: password.value 
-		});
+		const data = await response.json();
+		const token = data.access
+		console.log("RESPONSE TOKEN =======> ", token);
 		
-		await userStore.fetchUserDetails();
+		if (!token) {
+			throw new Error("No token received");
+		}
 		
-		// userStore.isAdmin ? router.push('/dashboard') : router.push('/');
+		localStorage.setItem("token", token);
+		userStore.token = token;
+		userStore.email = email.value;
 		
-  } catch (error) {
+		userStore.isAdmin ? router.push('/dashboard') : router.push('/');
+		
+	} catch (error) {
 		console.log("ERROR", error)
-    Swal.fire('Login Failed', error.response?.data?.message || 'Invalid credentials', 'error');
+    Swal.fire('Login Failed', 'Invalid credentials', 'error');
   }
-};
+}
+
+// const handleSubmit = async () => {
+//   try {	
+// 		const res = await api.post("/users/login", { 
+// 			email: email.value, 
+// 			password: password.value
+// 		});
+		
+// 		console.log("LOGIN RES =======> ", res);
+		
+// 		if (!res.status === 200) {
+// 			throw new Error("Failed to login")
+// 		}
+		
+// 		await userStore.loginUser({ 
+// 			email: email.value, 
+// 			password: password.value 
+// 		});
+		
+// 		await userStore.fetchUserDetails();
+		
+// 		userStore.isAdmin ? router.push('/dashboard') : router.push('/');
+		
+//   } catch (error) {
+// 		console.log("ERROR", error)
+//     Swal.fire('Login Failed', error.response?.data?.message || 'Invalid credentials', 'error');
+//   }
+// };
 
 </script>
 
@@ -55,7 +96,7 @@ const handleSubmit = async () => {
 		<h1 class="my-5 text-warning text-center">Login</h1>
 		<div class="row d-flex justify-content-center">
 			<div class="col-md-5 border shadow-sm rounded-3 mx-auto p-5 bg-light">
-				<form @submit.prevent="handleSubmit">
+				<form @submit.prevent="loginWithFetch">
 					<div class="mb-3">
 						<label for="emailInput"	class="form-label">Email Address</label>
 						<input type="email"	class="form-control border-dark" id="emailInput" v-model="email"/>
