c3df0736565155dbece2660d8177b0a757768f5a
Junfel <junfeldev@gmail.com>
2025-08-02 09:19:48 +0800

    Update navbar states

--- src/components/NavbarComponent.vue
+++ src/components/NavbarComponent.vue
@@ -1,14 +1,14 @@
 <script setup>
-  import { onBeforeMount } from 'vue';
-  import { useUserStore } from '@/stores/userStore';
+import { useUserStore } from '@/stores/userStore';
+import { onMounted } from 'vue';
 
-  const userStore = useUserStore();
-  
-  onBeforeMount( async () => {
-    if (userStore.token && userStore.token !== "null") {
-      await userStore.fetchUserDetails();
-    }
-  });
+const userStore = useUserStore();
+
+onMounted(() => {
+  if (userStore.token) {
+    userStore.fetchUserDetails();  // ← this restores email/isAdmin!
+  }
+});
 </script>
 
 <template>
@@ -19,32 +19,17 @@
         <div class="ms-2 d-inline text-secondary">Cap<span class="text-warning">CAKES</span></div>
       </router-link>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+        <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
         <div class="navbar-nav ms-auto gap-4">
-          <router-link :to="{ name: 'Products' }" class="nav-link">
-            Products
-          </router-link>
-          <router-link :to="{ name: 'Cart' }" class="nav-link" 
-            v-if="!userStore.isAdmin && userStore.email">
-            Cart
-          </router-link>
-         <router-link :to="{ name: 'Profile' }" class="nav-link" v-if="userStore.email">
-            Profile
-          </router-link> 
-          <router-link :to="{ name: 'Dashboard' }" class="nav-link" v-if="userStore.isAdmin">
-            Dashboard
-          </router-link>
-          <router-link :to="{ name: 'Register' }" class="nav-link" v-if="!userStore.email">
-            Register
-          </router-link>
-          <router-link :to="{ name: 'Login' }" class="nav-link" v-if="!userStore.email">
-            Login
-          </router-link>
-          <router-link :to="{ name: 'Logout' }" class="nav-link" v-else>
-            Logout
-          </router-link>
+          <router-link :to="{ name: 'Products' }" class="nav-link"> Products </router-link>
+          <router-link :to="{ name: 'Cart' }" class="nav-link" v-if="userStore.email && !userStore.isAdmin"> Cart </router-link>
+          <router-link :to="{ name: 'Profile' }" class="nav-link" v-if="userStore.email"> Profile </router-link> 
+          <router-link :to="{ name: 'Dashboard' }" class="nav-link" v-if="userStore.isAdmin"> Dashboard </router-link>
+          <router-link :to="{ name: 'Register' }" class="nav-link" v-if="!userStore.email"> Register </router-link>
+          <router-link :to="{ name: 'Login' }" class="nav-link" v-if="!userStore.email"> Login </router-link>
+          <router-link :to="{ name: 'Logout' }" class="nav-link" v-else> Logout </router-link>
         </div>
       </div>
   	</div>
