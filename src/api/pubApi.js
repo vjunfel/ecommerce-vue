5054b59dc480357d62e26ddd47673f1bad080122
Junfel <junfeldev@gmail.com>
2025-08-04 21:26:00 +0800

    Hardcode baseURL since env is not working.

--- src/api/pubApi.js
+++ src/api/pubApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const VITE_API_BASE_URL = import.meta.process.env.VITE_JOB_TRACKER_API;
-// const VITE_API_BASE_URL = "https://vvro2vmufk.execute-api.us-west-2.amazonaws.com/production/";
+// const VITE_API_BASE_URL = import.meta.process.env.VITE_JOB_TRACKER_API;
+const VITE_API_BASE_URL = "https://vvro2vmufk.execute-api.us-west-2.amazonaws.com/production/";
 
 const publicApi = axios.create({
   baseURL: VITE_API_BASE_URL,
